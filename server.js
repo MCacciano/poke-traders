db6e848e55ec1ca59273e8c88c3e9164eed9a567
Michael Cacciano <michael.cacciano@gmail.com>
2020-01-05 00:13:50 -0500

    mongodb connected, morgan added for dev middleware logging

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
+// load env vars
 dotenv.config({ path: './config/.config.env' });
 
+// connect DB
+connectDB();
+
+// init express
 const app = express();
+
+// body parser
+app.use(express.json());
+
+// dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,4 +27,11 @@ app.get('/', (req, res) => {
   res.send('testing');
 });
 
-app.listen(PORT, console.log(`Server running on port: ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
