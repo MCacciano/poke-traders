a7228a3d3b39b079c3b7f0102e92c94c287c3819
mcacciano <michael.cacciano@gmail.com>
2020-01-08 07:36:13 -0500

    added pokemon pokeID and pokeApiUrl to Pokemon mode - updated all models to reflect thisl

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,31 +1,13 @@
-const pokeApi = require('../axios/pokeApi');
+// const pokeApi = require('../axios/pokeApi');
 const asyncHandler = require('../middleware/asyncHandler');
 
 const Pokemon = require('../models/Pokemon');
 
 exports.getAllPokemon = asyncHandler(async (req, res, next) => {
-  // const response = await pokeApi.get('/pokemon?limit=1000');
+  // utility for padding pokemon id to get image asset
+  // const padIdToThree = id => (id <= 99999 ? `00${id}`.slice(-3) : id);
+
   const results = await Pokemon.find();
 
   res.status(200).json({ success: true, count: results.length, results });
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  // ALL OF THIS WAS TO SEED THE DB WITH POKEMON
-  // const padIdToThree = id => (id <= 99999 ? `00${id}`.slice(-3) : id);
-  // const pokeID = url => padIdToThree(url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/', ''));
-
-  // const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokeID()}.png`;
-
-  // response.data.results.forEach((pokemon, i) => {
-  // Pokemon.create({
-  //   ...pokemon,
-  //   imageUrl: `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokeID(pokemon.url)}.png`
-  // });
-
-  // console.log({
-  //   ...pokemon,
-  //   imageUrl: `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokeID(pokemon.url)}.png`
-  // });
-  // });
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 });
